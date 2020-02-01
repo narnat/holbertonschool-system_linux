0dcd152e2b0130d05daa37ce011f240e8ff465e7
Farrukh <farruh1996@gmail.com>
2020-02-01 14:46:37 -0800

    Change the name of program in error()

--- 0x00-ls/error.c
+++ 0x00-ls/error.c
@@ -25,23 +25,23 @@ void free_arr(container_t *files, int size)
 void error(args_t *args)
 {
 	char buf[BUFSIZ];
-	char *name = args->argv[0];
+	char *name = "hls";
 
-	fprintf(stderr, "%s: ", name);
-	if (args->err == OPT_ERR)
-	{
-		sprintf(buf, "invalid option -- '%c'\nTry", args->argv[args->inv][1]);
-		fprintf(stderr, "%s 'ls --help' for more information.\n", buf);
-		exit(LS_SERIOUS);
-	}
-	if (args->err == FILE_ERR)
-	{
-		sprintf(buf, "cannot access %s", args->argv[args->inv]);
-		perror(buf);
-	}
-	if (args->err == P_ERR)
-	{
-		sprintf(buf, "cannot open directory %s", args->dir_name);
-		args->err = LS_SERIOUS, perror(buf);
-	}
+    fprintf(stderr, "%s: ", name);
+    if (args->err == OPT_ERR)
+    {
+        sprintf(buf, "invalid option -- '%c'\nTry", args->argv[args->inv][1]);
+        fprintf(stderr, "%s 'ls --help' for more information.\n", buf);
+        exit(LS_SERIOUS);
+    }
+    if (args->err == FILE_ERR)
+    {
+        sprintf(buf, "cannot access %s", args->argv[args->inv]);
+        perror(buf);
+    }
+    if (args->err == P_ERR)
+    {
+        sprintf(buf, "cannot open directory %s", args->dir_name);
+        args->err = LS_SERIOUS, perror(buf);
+    }
 }
