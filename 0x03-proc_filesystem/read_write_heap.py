ecd33273236a0df6951414e113755fe59a2febb0
Farrukh <farruh1996@gmail.com>
2020-03-10 13:46:48 -0700

    Refractor code

--- 0x03-proc_filesystem/read_write_heap.py
+++ 0x03-proc_filesystem/read_write_heap.py
@@ -3,35 +3,40 @@ import sys
 
 ''' This script changes ASCII string in heap of the process '''
 
-try:
+
+def get_args():
+    '''
+    Parses arguments rand returns them
+    argv[1]: <pid>
+    argv[2]: <search_string>
+    argv[3]: <replace_string>
+    '''
+
     pid = int(sys.argv[1])
     search_string = sys.argv[2]
     replace_string = sys.argv[3]
     if len(replace_string) > len(search_string):
         raise IndexError
-except IndexError:
-    print("Usage: <read_write_heap.py> pid search_string replace_string")
-    exit(1)
-except ValueError:
-    print("PID should be a valid integer greater than 0")
-    exit(1)
+
+    return pid, search_string, replace_string
 
 
-maps_file = "/proc/{:d}/maps".format(pid)
-mem_file = "/proc/{:d}/mem".format(pid)
-search_word = "[heap]"
-props_line = {"start": None, "end": None, "perms": None}
+def read_write_file(pid, search_string, replace_string):
+    '''
+    Read mem file and swap the given string
+    '''
 
-try:
+    maps_file = "/proc/{:d}/maps".format(pid)
+    mem_file = "/proc/{:d}/mem".format(pid)
+    search_word = "[heap]"
+    props_line = {"start": None, "end": None, "perms": None}
+
     with open(maps_file, "r") as maps, open(mem_file, "rb+") as mem:
         for line in maps:
             if search_word in line:
                 print("[*] Found heap")
                 tmp = line.split(" ")
                 addr = tmp[0].split("-")
-                if len(addr) != 2:
-                    print("Wrong address format")
-                    exit(1)
                 props_line["start"] = int(addr[0], 16)
                 props_line["end"] = int(addr[1], 16)
                 props_line["perms"] = tmp[1]
@@ -48,14 +53,33 @@ try:
         mem.seek(props_line["start"] + idx)
         mem.write(bytes(replace_string + "\0", "ASCII"))
 
-except FileNotFoundError as e:
-    print(e, file=sys.stderr)
-    exit(1)
 
-except ValueError:
-    print("Could not find the string")
-    exit(1)
+def main():
+    '''
+    Entry point of the script
+    '''
+    try:
+        exit_value = 1
+        pid, search_string, replace_string = get_args()
+        read_write_file(pid, search_string, replace_string)
+        exit_value = 0
+    except IndexError:
+        print("Usage: <read_write_heap.py> pid search_string replace_string")
+        exit(1)
+    except ValueError:
+        print("PID should be a valid integer greater than 0")
+        exit(1)
+    except FileNotFoundError as e:
+        print(e)
+        exit(1)
+    except ValueError:
+        print("Could not find the string")
+        exit(1)
+    except PermissionError:
+        print("No permission")
+        exit(1)
+    finally:
+        exit(exit_value)
 
-except PermissionError:
-    print("No permission")
-    exit(1)
+if __name__ == '__main__':
+    main()
