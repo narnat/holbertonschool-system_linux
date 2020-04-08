4d23e545df85caca0612526f74dbca296c327c6d
Farrukh <farruh1996@gmail.com>
2020-04-07 21:55:15 -0700

    Add task 0

---
+++ 0x06-signals/0-handle_signal.c
@@ -0,0 +1,21 @@
+#include "signals.h"
+
+/**
+ * signal_handler - Action to take in case signal
+ * Return: 0 on success, or -1 on error
+*/
+void signal_handler(int signum)
+{
+	printf("Gotcha! [%d]\n", signum);
+}
+
+/**
+ * handle_signal - handles SIGINT signal
+ * Return: 0 on success, or -1 on error
+*/
+int handle_signal(void)
+{
+	if (signal(SIGINT, signal_handler) == SIG_ERR)
+		return (1);
+	return (0);
+}
