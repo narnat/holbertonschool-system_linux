ead5148d95ae9865684174a41aba582e75735219
Farrukh <farruh1996@gmail.com>
2020-06-09 20:36:49 -0700

    Add task 3

--- 0x09-strace/strace_3.c
+++ 0x09-strace/strace_3.c
@@ -6,14 +6,11 @@
 #include <sys/types.h>
 #include <sys/reg.h>
 
-int sub_process(int argc, char *argv[])
-{
-	(void) argc;
-	ptrace(PTRACE_TRACEME);
-	kill(getpid(), SIGSTOP);
-	return execve(argv[0], argv, NULL);
-}
-
+/**
+ * wait_syscall - Waits for syscall invocation in subprocess
+ * @child: subprocess PID
+ * Return: 0 if syscall is called, 0 if subprocess is terminated
+ */
 int wait_syscall(pid_t child)
 {
 	int status;
@@ -30,55 +27,78 @@ int wait_syscall(pid_t child)
 	return (1);
 }
 
-void print_register(struct user_regs_struct u_in, int idx, ulong reg, char *str)
+/**
+ * print_register - print registers (params of syscalls)
+ * @u_in: registers struct
+ * @idx: parameter index
+ * @reg: register
+ * @str: delimeter
+*/
+void print_register(struct user_regs_struct u_in,
+		    int idx, ulong reg, char *str)
 {
 	if (syscalls_64_g[u_in.orig_rax].params[idx] != (type_t)-1 &&
 	    syscalls_64_g[u_in.orig_rax].params[idx] != VOID)
 	{
 		if (syscalls_64_g[u_in.orig_rax].params[idx] == VARARGS)
-			fprintf(stderr, "%s...", str);
+			fprintf(stdout, "%s...", str);
 		else
-			fprintf(stderr, "%s%#lx", str, reg);
+			fprintf(stdout, "%s%#lx", str, reg);
 	}
 }
 
+/**
+ * print_args - print arguments to syscalls
+ * @u_in: registers struct
+*/
 void print_args(struct user_regs_struct u_in)
 {
-	fprintf(stderr, "(");
 	print_register(u_in, 0, u_in.rdi, "");
 	print_register(u_in, 1, u_in.rsi, ", ");
 	print_register(u_in, 2, u_in.rdx, ", ");
 	print_register(u_in, 3, u_in.r10, ", ");
 	print_register(u_in, 4, u_in.r8, ", ");
 	print_register(u_in, 5, u_in.r9, ", ");
-	fprintf(stderr, ")");
 }
 
+/**
+ * tracer - tracer process
+ * @child: subprocess PID
+ * Return: EXIT_SUCCESS on success
+ */
 int tracer(pid_t child)
 {
 	int status;
-	long syscall, retval;
+	long retval;
 	struct user_regs_struct u_in;
 
-	(void) syscall;
 	waitpid(child, &status, 0);
+	setbuf(stdout, NULL);
 	ptrace(PTRACE_SETOPTIONS, child, 0, PTRACE_O_TRACESYSGOOD);
 	while (1)
 	{
 		if (wait_syscall(child) != 0)
 			break;
-		syscall = ptrace(PTRACE_GETREGS, child, 0, &u_in);
-		fprintf(stderr, "%s", syscalls_64_g[u_in.orig_rax].name);
+		ptrace(PTRACE_GETREGS, child, 0, &u_in);
+		fprintf(stdout, "%s(", syscalls_64_g[u_in.orig_rax].name);
 		print_args(u_in);
-		if (wait_syscall(child) != 0) break;
+		if (wait_syscall(child) != 0)
+			break;
 		retval = ptrace(PTRACE_PEEKUSER, child, sizeof(long) * RAX);
-		fprintf(stderr, " = %#lx\n", retval);
+		fprintf(stdout, ") = %#lx\n", retval);
 	}
-	fprintf(stderr, " = ?\n");
+	fprintf(stdout, ") = ?\n");
 	return (EXIT_SUCCESS);
 }
 
-int main(int argc, char *argv[])
+/**
+ * main - Entry point
+ * @argc: Args count
+ * @argv: Args vector
+ * @envp: Environmental variables
+ * Return: EXIT_FAILURE if failed, EXIT_SUCCESS if successful
+ */
+int main(int argc, char *argv[], char *envp[])
 {
 	pid_t child;
 
@@ -92,7 +112,9 @@ int main(int argc, char *argv[])
 
 	if (child == 0)
 	{
-		return (sub_process(argc - 1, argv + 1));
+		ptrace(PTRACE_TRACEME);
+		kill(getpid(), SIGSTOP);
+		return (execve(argv[1], argv + 1, envp));
 	}
 	else
 	{
