0fe690c1ea4cdfb644e470026cc46d7342bbb9ab
Farrukh <farruh1996@gmail.com>
2020-06-09 20:05:49 -0700

    Add task 2

--- 0x09-strace/strace_2.c
+++ 0x09-strace/strace_2.c
@@ -3,14 +3,26 @@
 #include <sys/types.h>
 #include <sys/reg.h>
 
-int sub_process(int argc, char *argv[])
+/**
+ * sub_process - create a subprocess
+ * @argc: Args count
+ * @argv: Args vector
+ * @envp: Environmental variables
+ * Return: -1 if failed
+ */
+int sub_process(int argc, char *argv[], char *envp[])
 {
 	(void) argc;
 	ptrace(PTRACE_TRACEME);
 	kill(getpid(), SIGSTOP);
-	return execve(argv[0], argv, NULL);
+	return (execve(argv[0], argv, envp));
 }
 
+/**
+ * wait_syscall - Waits for syscall invocation in subprocess
+ * @child: subprocess PID
+ * Return: 0 if syscall is called, 0 if subprocess is terminated
+ */
 int wait_syscall(pid_t child)
 {
 	int status;
@@ -27,29 +39,42 @@ int wait_syscall(pid_t child)
 	return (1);
 }
 
+/**
+ * tracer - tracer process
+ * @child: subprocess PID
+ * Return: EXIT_SUCCESS on success
+ */
 int tracer(pid_t child)
 {
 	int status;
 	long syscall, retval;
 
 	waitpid(child, &status, 0);
-
+	setbuf(stdout, NULL);
 	ptrace(PTRACE_SETOPTIONS, child, 0, PTRACE_O_TRACESYSGOOD);
 	while (1)
 	{
 		if (wait_syscall(child) != 0)
 			break;
 		syscall = ptrace(PTRACE_PEEKUSER, child, sizeof(long) * ORIG_RAX);
-		fprintf(stderr, "%s = ", syscalls_64_g[syscall].name);
-		if (wait_syscall(child) != 0) break;
+		fprintf(stdout, "%s = ", syscalls_64_g[syscall].name);
+		if (wait_syscall(child) != 0)
+			break;
 		retval = ptrace(PTRACE_PEEKUSER, child, sizeof(long) * RAX);
-		fprintf(stderr, "%#lx\n", retval);
+		fprintf(stdout, "%#lx\n", retval);
 	}
-	fprintf(stderr, "?\n");
+	fprintf(stdout, "?\n");
 	return (EXIT_SUCCESS);
 }
 
-int main(int argc, char *argv[])
+/**
+ * main - Entry point
+ * @argc: Args count
+ * @argv: Args vector
+ * @envp: Environmental variables
+ * Return: EXIT_FAILURE if failed, EXIT_SUCCESS if successful
+ */
+int main(int argc, char *argv[], char *envp[])
 {
 	pid_t child;
 
@@ -61,9 +86,13 @@ int main(int argc, char *argv[])
 
 	child = fork();
 
-	if (child == 0)
+	if (child < 0)
 	{
-		return (sub_process(argc - 1, argv + 1));
+		return (EXIT_FAILURE);
+	}
+	else if (child == 0)
+	{
+		return (sub_process(argc - 1, argv + 1, envp));
 	}
 	else
 	{
