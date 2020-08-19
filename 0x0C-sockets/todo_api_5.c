c985c7689c1661880a94df862e819338abfdd629
Farrukh Akhrarov <farruh1996@gmail.com>
2020-08-19 14:58:10 -0700

    Remove duplicate of send_all_todos()

--- 0x0C-sockets/todo_api_5.c
+++ 0x0C-sockets/todo_api_5.c
@@ -1,40 +1,6 @@
 #include "rest_api.h"
 
 /**
- * send_all_todos - send all todos as JSON
- * @sck: socketr fd
- * @queue: queue of todos
- * Return: 0 on success
- */
-int send_all_todos(int sck, queue_t *queue)
-{
-	size_t bytes = 0, size;
-	char buf[10 * BUFSIZ];
-	todo_list *node;
-
-	size = queue->size;
-	node = queue->first;
-	while (node)
-	{
-		serialize(buf + bytes, 10 * BUFSIZ - bytes, node);
-		bytes += strlen(buf + bytes);
-		node = node->next;
-		--size;
-		if (size)
-		{
-			*(buf + bytes) = ',';
-			++bytes;
-		}
-	}
-
-	dprintf(sck,
-			"HTTP/1.1 200 OK\r\n"
-			"Content-Length: %lu\r\nContent-Type: application/json\r\n\r\n[%s]",
-			strlen(buf) + 2, buf);
-	return (0);
-}
-
-/**
  * parser - parse HTTP request
  * @header: header where queries are stored
  * @sck: socket fd
