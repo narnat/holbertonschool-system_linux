278b2a139188129056a1bb4de14acad3469b4d46
Farrukh Akhrarov <farruh1996@gmail.com>
2020-08-19 11:35:35 -0700

    Add support for GET method

---
+++ 0x0C-sockets/todo_api_5.c
@@ -0,0 +1,130 @@
+#include "rest_api.h"
+
+/**
+ * send_all_todos - send all todos as JSON
+ * @sck: socketr fd
+ * @queue: queue of todos
+ * Return: 0 on success
+ */
+int send_all_todos(int sck, queue_t *queue)
+{
+	size_t bytes = 0, size;
+	char buf[10 * JSON_SIZE];
+	todo_list *node;
+
+	size = queue->size;
+	node = queue->first;
+	while (node)
+	{
+		serialize(buf + bytes, 10 * JSON_SIZE - bytes, node);
+		bytes += strlen(buf + bytes);
+		node = node->next;
+		--size;
+		if (size)
+		{
+			*(buf + bytes) = ',';
+			++bytes;
+		}
+	}
+
+	dprintf(sck,
+			"HTTP/1.1 200 OK\r\n"
+			"Content-Length: %lu\r\nContent-Type: application/json\r\n\r\n[%s]",
+			strlen(buf) + 2, buf);
+	return (0);
+}
+
+/**
+ * parser - parse HTTP request
+ * @header: header where queries are stored
+ * @sck: socket fd
+ * @queue: queue of todo tasks
+ * Return: 0 on success, -1 on failure
+*/
+int parser(char *header, int sck, queue_t *queue)
+{
+	char method[FIELD_SIZE] = {0}, path[FIELD_SIZE] = {0}, *body;
+	char *crlf = "\r\n\r\n", *content_len, *title, *desc;
+	int bytes;
+
+	bytes = sscanf(header, "%s %s", method, path);
+	if (!strcasecmp(method, "GET"))
+		return (send_all_todos(sck, queue));
+	if (strcasecmp(method, "POST") || bytes == EOF)
+	{
+		send_err(sck, 404);
+		return (-1);
+	}
+	if (strcmp(path, "/todos"))
+	{
+		send_err(sck, 404);
+		return (-1);
+	}
+	body = strstr(header, crlf);
+	*body = '\0', body += strlen(crlf);
+	content_len = strstr(header, "Content-Length");
+	if (!content_len)
+	{
+		send_err(sck, 411);
+		return (-1);
+	}
+	if (get_queries(sck, body, &title, &desc) == -1)
+		return (-1);
+	en_queue(queue, title, desc);
+	send_created(sck, queue);
+	return (0);
+}
+
+/**
+ * accept_connection - wait for connections from users and accept them
+ * @sck: descriptor of the passive socket
+ * @queue: queue of todo tasks
+ * Return: -1 if failed, 0 on success
+*/
+int accept_connection(int sck, queue_t *queue)
+{
+	int new_sck, clt_size;
+	struct sockaddr_in client;
+	char header[BUFSIZ] = {0};
+
+	clt_size = sizeof(client);
+	while (1)
+	{
+		new_sck = accept(sck, (struct sockaddr *)&client, (socklen_t *)&clt_size);
+		if (new_sck < 0)
+			return (-1);
+		printf("Client connected: %s\n", inet_ntoa(client.sin_addr));
+		if (print_raw_req(header, new_sck) == -1)
+		{
+			close(new_sck);
+			close(sck);
+			return (-1);
+		}
+		parser(header, new_sck, queue);
+		close(new_sck);
+	}
+	return (0);
+}
+
+/**
+ * main - todo REST API
+ * Return: 0 on success, 1 on failure
+ */
+int main(void)
+{
+	int sck;
+	queue_t *queue;
+
+	queue = create_queue();
+	if (!queue)
+		return (EXIT_FAILURE);
+	setbuf(stdout, NULL);
+	sck = init_inet_socket();
+	if (sck == -1)
+		return (EXIT_FAILURE);
+	if (accept_connection(sck, queue) == -1)
+		return (EXIT_FAILURE);
+	close(sck);
+	free_queue(&queue);
+	return (EXIT_SUCCESS);
+}
